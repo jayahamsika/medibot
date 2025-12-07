12c90d1b5a3cb89077e27339c6535b9b587e4a8d
Jaya Hamsika P <jayahamsika1@gmail.com>
2025-12-07 22:21:10 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'medical_chatbot',
+    version = '0.1.0',
+    author = 'Jaya Hamsika',
+    author_email = 'jayahamsika1@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+    )
